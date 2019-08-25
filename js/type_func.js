5af10fa82b082eb616c72f206388d55426959d55
minhle <ngocminh58@gmail.com>
2019-08-25 10:21:58 +0700

    function

--- js/type_func.js
+++ js/type_func.js
@@ -1,18 +1,46 @@
-
 // var
 // const
 // let
 
 // var name = 'Thiem';
 
+// console.log('name', name);
+// getName();
+// console.log('name', name);
 
+//  function getName() {
+//   name = 'Minh';
+//   // var name = "Minh"
+//   console.log('name in func', name);
+// }
 
-console.log('name', name);
-getName();
-console.log('name', name);
+const greeting = function(params) {
+  console.log("hi i'm anonymous func");
+};
 
- function getName() {
-  name = 'Minh';
-  // var name = "Minh"
-  console.log('name in func', name);
+// arrow func
+const getNameArrow = (person = 'Thiem') => {
+  console.log(' arrow this is: ', this);
+};
+
+function getName(person) {
+  console.log('this is: ', this);
 }
+
+// bind this in function
+a = {
+  getNameArrow: (person = 'Thiem') => {
+    console.log(' arrow this is: ', this);
+  },
+  getName: function(person) {
+    console.log('this is: ', this);
+  }
+};
+
+a.getNameArrow();
+a.getName();
+
+
+(function (person) {
+  console.log('name: ', person);
+})('name')
