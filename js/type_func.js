45c721a74ac00c0f1059c06afe79f09f865acba3
Minh Le <minhle@Minhs-MacBook-Pro.local>
2019-08-25 09:16:00 +0700

    varible

--- js/type_func.js
+++ js/type_func.js
@@ -1,1 +1,18 @@
-console.log('hello')
+
+// var
+// const
+// let
+
+// var name = 'Thiem';
+
+
+
+console.log('name', name);
+getName();
+console.log('name', name);
+
+ function getName() {
+  name = 'Minh';
+  // var name = "Minh"
+  console.log('name in func', name);
+}
