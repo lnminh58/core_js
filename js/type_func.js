3b86fbadea58227f24dc5be7069b559398dd82ac
minhle <ngocminh58@gmail.com>
2019-08-25 10:53:47 +0700

    pass params

--- js/type_func.js
+++ js/type_func.js
@@ -14,33 +14,44 @@
 //   console.log('name in func', name);
 // }
 
-const greeting = function(params) {
-  console.log("hi i'm anonymous func");
+// const greeting = function(params) {
+//   console.log("hi i'm anonymous func");
+// };
+
+// // arrow func
+// const getNameArrow = (person = 'Thiem') => {
+//   console.log(' arrow this is: ', this);
+// };
+
+// function getName(person) {
+//   console.log('this is: ', this);
+// }
+
+// // bind this in function
+// a = {
+//   getNameArrow: (person = 'Thiem') => {
+//     console.log(' arrow this is: ', this);
+//   },
+//    getName: function(praram) {
+//     console.log('this is: ', this, );
+//   }
+// };
+
+// a.getNameArrow();
+// a.getName();
+
+// (function (person) {
+//   console.log('name: ', person);
+// })('name')
+
+const passParamsFunc = (params, callback) => {
+  console.log(params, callback);
+  console.log('name is: ', params.name);
+  console.log('age is: ', params.age);
+  console.log('address is: ', params.address);
+  callback();
 };
 
-// arrow func
-const getNameArrow = (person = 'Thiem') => {
-  console.log(' arrow this is: ', this);
-};
+const doWhileDone = () => console.log('done!!!!')
 
-function getName(person) {
-  console.log('this is: ', this);
-}
-
-// bind this in function
-a = {
-  getNameArrow: (person = 'Thiem') => {
-    console.log(' arrow this is: ', this);
-  },
-  getName: function(person) {
-    console.log('this is: ', this);
-  }
-};
-
-a.getNameArrow();
-a.getName();
-
-
-(function (person) {
-  console.log('name: ', person);
-})('name')
+passParamsFunc({ name: 'John', age: 18, address: 'washington' }, doWhileDone);
