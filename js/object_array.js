35cc795c93136b9b5a75fa57a9eb16a9a25730d8
minhle <ngocminh58@gmail.com>
2019-08-27 18:38:24 +0700

    re-structure

--- js/object_array.js
+++ js/object_array.js
@@ -1,1 +1,12 @@
 //object
+
+const JohnPerson = {
+  name: 'John',
+  age: 18,
+  address: 'Washington, D.C',
+  sex: null,
+  parent: undefined
+};
+
+// const johnParent = JohnPerson.parent || { name: 'Jack'} // &&
+console.log(JohnPerson);
