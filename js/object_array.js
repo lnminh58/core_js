481c48b92402222bfc51a74588b312c73aa361e7
minhle <ngocminh58@gmail.com>
2019-08-27 19:09:22 +0700

    object done

--- js/object_array.js
+++ js/object_array.js
@@ -5,8 +5,50 @@ const JohnPerson = {
   age: 18,
   address: 'Washington, D.C',
   sex: null,
-  parent: undefined
+  parent: undefined,
+  friend: {
+    boy: 'hell'
+  },
+  'school.class': '10A',
+  introduce: function(isFull) {
+    return !!isFull
+      ? `my name is ${this.name}, i'am ${this.age}. My address is ${this.address}`
+      : `My name is ${this.name}`;
+  },
+
+  greeting: () => `arrow func, cann't see my name ${this.name}`
 };
 
 // const johnParent = JohnPerson.parent || { name: 'Jack'} // &&
+// console.log(JohnPerson.introduce());
+// console.log(JohnPerson.greeting());
+
+// // reference object
+
+// // const JackPerson = johnPerson
+// const JackPerson = {
+//   ...JohnPerson,
+//   friend: {
+//     girl: 'ana',
+//   }
+// };
+// JackPerson.age = 19;
+// JackPerson.friend.boy = 'holl'
+// console.log(JackPerson, JohnPerson);
+
+console.log(JohnPerson['name']);
 console.log(JohnPerson);
+console.log(Object.keys(JohnPerson));
+Object.keys(JohnPerson).forEach(function(item, index) {
+  console.log(item, index);
+  console.log(JohnPerson[item]);
+});
+
+
+console.log(Object.values(JohnPerson))
+
+const arrField = Object.keys(JohnPerson).map(item => ({
+  [item]: JohnPerson[item]
+}))
+
+console.log(arrField)
